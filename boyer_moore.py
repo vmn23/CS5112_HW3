9ff7bc3a0dcf468ba916429a73f8c78451c7d168
Sung <sp2528@cornell.edu>
2018-10-16 11:43:32 -0400

    Update boyer_moore.py

--- boyer_moore.py
+++ boyer_moore.py
@@ -1,3 +1,10 @@
+###########################################################################
+# CS 5112 HW 3 - Huffman and Boyer-Moore
+# Authors: Sungseo Park and Varun Narayan
+# Python 2.7
+# Date: 10/16/18
+###########################################################################
+
 # Implementation of the Boyer-Moore majority vote algorithm.
 #
 # Given a list of elements `l`, one would use this class in the following way
@@ -17,6 +24,13 @@ class BoyerMooreMajority:
   def add_next_element(self, element):
     assert(element is not None)
     #YOUR CODE HERE
+    if self.counter == 0:
+    	self.guess = element
+    	self.counter = 1
+    elif self.guess == element:
+    	self.counter += 1
+    else:
+    	self.counter -= 1
 
   # Gives the best guess of which of the elements seen so far make up the
   # majority of the elements in set of elements. If a majority element exists,
@@ -24,4 +38,5 @@ class BoyerMooreMajority:
   # about the output.
   def get_majority(self):
     #YOUR CODE HERE
+    return self.guess
     pass
